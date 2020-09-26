ee14323995ad3215abce8663410ede91a028f4b2
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2020-09-26 12:57:12 +0200

    Update Startup.cs

--- PocketPharmacy/PocketPharmacy/Startup.cs
+++ PocketPharmacy/PocketPharmacy/Startup.cs
@@ -8,12 +8,12 @@ namespace PocketPharmacy
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -35,10 +35,7 @@ namespace PocketPharmacy
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
