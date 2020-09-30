ee13f3a4ec123253b0e355ce1bf6751634a411bf
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2020-09-30 11:48:21 +0200

    add main domain models.
    Medicine and Dosage classes.

---
+++ PocketPharmacy/PocketPharmacy/Core/Models/Dosage.cs
@@ -0,0 +1,9 @@
+﻿namespace PocketPharmacy.Core.Models
+{
+    public class Dosage
+    {
+        public int PerDays { get; set; }
+        public double Amount { get; set; }
+        public string Unit { get; set; }
+    }
+}
---
+++ PocketPharmacy/PocketPharmacy/Core/Models/Medicine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PocketPharmacy.Core.Models
+{
+    public class Medicine
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Amount { get; set; }
+        public string Unit { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public bool NeedPrescription { get; set; }
+        public Dosage Dosage { get; set; }
+
+        public double GetWeeklyDosage()
+        {
+            return 0;
+        }
+
+        public bool IsExpired()
+        {
+            return false;
+        }
+
+        public bool HasWeeklyDosage()
+        {
+            return false;
+        }
+    }
+}
