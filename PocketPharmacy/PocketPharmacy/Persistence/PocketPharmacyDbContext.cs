1763081119b8735ff4089dedb5c96b1c49486656
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2020-09-30 12:07:36 +0200

    add a basic DbContext.

---
+++ PocketPharmacy/PocketPharmacy/Persistence/PocketPharmacyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PocketPharmacy.Core.Models;
+
+namespace PocketPharmacy.Persistence
+{
+    public class PocketPharmacyDbContext : DbContext
+    {
+        public DbSet<Medicine> Medicines { get; set; }
+
+        public PocketPharmacyDbContext(DbContextOptions<PocketPharmacyDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
