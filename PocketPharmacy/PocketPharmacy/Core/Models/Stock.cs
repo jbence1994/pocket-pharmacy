1ef20d56b006fc925d12a1af4c2c03808035536f
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2020-10-10 15:13:56 +0200

    add other properties to Stock model.

--- PocketPharmacy/PocketPharmacy/Core/Models/Stock.cs
+++ PocketPharmacy/PocketPharmacy/Core/Models/Stock.cs
@@ -1,9 +1,13 @@
-﻿namespace PocketPharmacy.Core.Models
+﻿using System;
+
+namespace PocketPharmacy.Core.Models
 {
     public class Stock
     {
         public int Id { get; set; }
         public User User { get; set; }
         public Medicine Medicine { get; set; }
+        public DateTime RecordingDate { get; set; }
+        public int Quantity { get; set; }
     }
 }
