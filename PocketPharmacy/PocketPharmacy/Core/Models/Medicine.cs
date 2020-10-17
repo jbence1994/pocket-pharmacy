21f01b65e1fb42850df0e37b832d1eb02e51fcc6
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2020-10-17 22:36:31 +0200

    remove unnecessary properties.

--- PocketPharmacy/PocketPharmacy/Core/Models/Medicine.cs
+++ PocketPharmacy/PocketPharmacy/Core/Models/Medicine.cs
@@ -5,7 +5,6 @@ namespace PocketPharmacy.Core.Models
     public class Medicine
     {
         public int Id { get; set; }
-        public User User { get; set; }
         public int UserId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
--- PocketPharmacy/PocketPharmacy/Core/Models/User.cs
+++ PocketPharmacy/PocketPharmacy/Core/Models/User.cs
@@ -1,17 +1,9 @@
-﻿using System.Collections.Generic;
-
-namespace PocketPharmacy.Core.Models
+﻿namespace PocketPharmacy.Core.Models
 {
     public class User
     {
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
-        public IList<Medicine> Medicines { get; set; }
-
-        public User()
-        {
-            Medicines = new List<Medicine>();
-        }
     }
 }
