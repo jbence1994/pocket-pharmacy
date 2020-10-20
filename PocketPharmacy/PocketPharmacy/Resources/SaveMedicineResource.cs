1c8c51e35aedaa7727d0e8b824b7063cb5381cd4
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2020-10-20 10:22:29 +0200

    Update SaveMedicineResource.cs

--- PocketPharmacy/PocketPharmacy/Resources/SaveMedicineResource.cs
+++ PocketPharmacy/PocketPharmacy/Resources/SaveMedicineResource.cs
@@ -4,6 +4,7 @@ namespace PocketPharmacy.Resources
 {
     public class SaveMedicineResource
     {
+        public int UserId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public double Amount { get; set; }
