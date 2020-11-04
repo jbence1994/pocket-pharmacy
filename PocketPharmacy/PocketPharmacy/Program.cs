09d79fa3b66515708922986c3ebf52d57b8189b7
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2020-11-04 18:51:06 +0100

    Update Program.cs

--- PocketPharmacy/PocketPharmacy/Program.cs
+++ PocketPharmacy/PocketPharmacy/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Hosting;
 
 namespace PocketPharmacy
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
